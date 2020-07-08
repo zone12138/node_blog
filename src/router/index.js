23ba97d06cdce33f5a581822c55836ecb3abef6c
xie <1459547902@qq.com>
2020-07-08 19:47:14 +0800

    添加说明

--- src/router/index.js
+++ src/router/index.js
@@ -58,6 +58,7 @@ const routes = [
 
 const router = new VueRouter({
   mode: 'history',
+  // 生产环境下, base 属性的值需要与 vue.config.js 文件中的 assetsDir 属性的值保持一致, 否则请求的路由页面的资源(如 css, js 文件)会报错(404)
   base: process.env.NODE_ENV === 'prodution' ? '/article/' : process.env.BABEL_URL,
   // base: '/app/',
   routes
